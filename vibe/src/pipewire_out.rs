d97c8ce7363abaa7c72a446ce91715a75d8f192e
Geoff Clements <ro2kz0@gmail.com>
2026-06-23 18:57:51 +0100

    vibe: update skip_offset_ms on skip request

--- vibe/src/pipewire_out.rs
+++ vibe/src/pipewire_out.rs
@@ -222,6 +222,7 @@ impl AudioOutput for PipewireAudioOutput {
                         let actual_skip_time =
                             decoder.samples_to_dur((bytes_to_skip / size_of::<f32>()) as _);
                         duration.fetch_add(actual_skip_time.as_millis() as _);
+                        skip_offset_ms.fetch_add(actual_skip_time.as_millis() as _);
                         skip_time = skip_time.saturating_sub(actual_skip_time);
 
                         if audio_buf.is_empty() {
